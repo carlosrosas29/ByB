cfeb7a1f497a01ba229499a3e14fcc7bd6ad37e3
carlosrosas29 <crosas0@ucol.mx>
2018-02-23 22:06:58 -0600

    Initial commit

---
+++ ByB.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnManipular = new System.Windows.Forms.Button();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.lblDirec = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnManipular
+            // 
+            this.btnManipular.Location = new System.Drawing.Point(12, 24);
+            this.btnManipular.Name = "btnManipular";
+            this.btnManipular.Size = new System.Drawing.Size(75, 23);
+            this.btnManipular.TabIndex = 0;
+            this.btnManipular.Text = "Manipular";
+            this.btnManipular.UseVisualStyleBackColor = true;
+            this.btnManipular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(120, 24);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 20);
+            this.txtNum.TabIndex = 1;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(117, 56);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(17, 13);
+            this.lbl3.TabIndex = 2;
+            this.lbl3.Text = "B:";
+            // 
+            // lbl7
+            // 
+            this.lbl7.AutoSize = true;
+            this.lbl7.Location = new System.Drawing.Point(23, 95);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(16, 13);
+            this.lbl7.TabIndex = 3;
+            this.lbl7.Text = "7:";
+            // 
+            // lbl6
+            // 
+            this.lbl6.AutoSize = true;
+            this.lbl6.Location = new System.Drawing.Point(23, 117);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(16, 13);
+            this.lbl6.TabIndex = 4;
+            this.lbl6.Text = "6:";
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Location = new System.Drawing.Point(116, 147);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(0, 13);
+            this.lblNivel.TabIndex = 5;
+            // 
+            // lblDirec
+            // 
+            this.lblDirec.AutoSize = true;
+            this.lblDirec.Location = new System.Drawing.Point(116, 179);
+            this.lblDirec.Name = "lblDirec";
+            this.lblDirec.Size = new System.Drawing.Size(0, 13);
+            this.lblDirec.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nivel del tanque:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Direccion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 363);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDirec);
+            this.Controls.Add(this.lblNivel);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.btnManipular);
+            this.Name = "Form1";
+            this.Text = "ByB";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnManipular;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lblNivel;
+        private System.Windows.Forms.Label lblDirec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ByB.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(txtNum.Text);
+            string txt, sensor;
+            text(out txt, out sensor);
+            string NivelTanque = text2();
+            char b;
+            string cadena1;
+            cadena1 = Convert.ToString(num, 2);
+            string refi = cadena1;
+            int tamaño = cadena1.Length;
+            int contador = 0;
+            for (int i = tamaño; i <= 7; i++)
+            {
+                contador++;
+                txt += "0";
+            }
+            txt += refi;
+            lbl3.Text = txt;
+            for (int j = 0; j <= 7; j++)
+            {
+                b = txt[j];
+                if (j == 2 || j == 3)
+                {
+                    sensor += txt[j];
+                }
+                else
+                {
+                    if (j >= 4 && j < 7)
+                    {
+                        NivelTanque += txt[j];
+                    }
+                    if (j == 0)
+                    {
+                        if (b == '1')
+                        {
+                            lbl7.Text += "Encendido";
+                        }
+                        else
+                        {
+                            if (b == '0')
+                            {
+                                lbl7.Text += "Apagado";
+                            }
+
+                        }
+                    }
+                    if (j == 1)
+                    {
+                        if (b == '1')
+                        {
+                            lbl6.Text += "Encendido";
+                        }
+                        else
+                        {
+                            if (b == '0')
+                            {
+                                lbl6.Text += "Apagado";
+                            }
+
+                        }
+
+                    }
+
+                    switch (sensor)
+                    {
+                        case "00":
+                            lblNivel.Text = "Vacio";
+                            break;
+                        case "01":
+                            lblNivel.Text = "Medio";
+                            break;
+                        case "10":
+                            lblNivel.Text = "lleno";
+                            break;
+                        case "11":
+                            lblNivel.Text = "En proceso de llenado";
+                            break;
+                    }
+
+                    switch (NivelTanque)
+                    {
+                        case "000":
+                            lblDirec.Text = "N";
+                            break;
+                        case "001":
+                            lblDirec.Text = "NE";
+                            break;
+                        case "010":
+                            lblDirec.Text = "E";
+                            break;
+                        case "011":
+                            lblDirec.Text = "SE";
+                            break;
+                        case "100":
+                            lblDirec.Text = "S";
+                            break;
+                        case "101":
+                            lblDirec.Text = "SO";
+                            break;
+                        case "110":
+                            lblDirec.Text = "O";
+                            break;
+                        default:
+                            if (NivelTanque == "111")
+                            {
+                                lblDirec.Text = "NO";
+                            }
+
+                            break;
+                    }
+                }
+
+            }
+
+        }
+
+        private static string text2()
+        {
+            return "";
+        }
+
+        private static void text(out string txt, out string sensor)
+        {
+            txt = "";
+            sensor = "";
+        }
+    }
+}
+
